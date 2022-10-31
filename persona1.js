0ed4f7ba374a4fb2fae1fadbcb5c1bb8bcb84df9
Cristina Oprisan <oprisan.cristina29@gmail.com>
2022-10-31 12:55:15 +0100

    clase persona archivos 1

---
+++ persona1.js
@@ -0,0 +1,45 @@
+let persona1 = {
+    nombre: `Santiago`,
+    apellido: `Jorje`,
+    anyoNacimiento: 1988,
+    aficiones: [`leer`, `fotografiar`, `ver peliculas`, `pintar`],
+    dni: {
+        anyoExpedicion: 2016,
+        lugarNacimiento: `Bilbao`
+    },
+    colorPelo: `Castano`
+}
+
+let persona2 = {
+    nombre: `Martinez`,
+    apellido: `Ana`,
+    anyoNacimiento: 2005,
+    aficiones: [`comer`, `pasear`, `dormir`],
+    dni: {
+        anyoExpedicion: 2020,
+        lugarNacimiento: `Madrid`
+    },
+    colorPelo: `Rojo`
+}
+let persona3 = {
+    nombre: `Perez`,
+    apellido: `Jose`,
+    anyoNacimiento: 2007,
+    aficiones: [`leer`, `ver peliculas`, `comer`, `cantar`],
+    dni: {
+        anyoExpedicion: 2000,
+        lugarNacimiento: `Barcelona`
+    },
+    colorPelo: `Negro`
+}
+let persona4 = {
+    nombre: `Ramirez`,
+    apellido: `Miguel`,
+    anyoNacimiento: 1980,
+    aficiones: [`leer`, `ver peliculas`, `escribir`, `bailar`],
+    dni: {
+        anyoExpedicion: 2014,
+        lugarNacimiento: `Santander`
+    },
+    colorPelo: `Ambar`
+}
---
+++ persona2.js
@@ -0,0 +1,23 @@
+let persona1 = {
+    nombre: `Oprisan`,
+    apellido: `Cristina`,
+    anyoNacimiento: 1988,
+    aficiones: [`leer`, `fotografiar`, `ver peliculas`, `pintar`],
+    dni: {
+        anyoExpedicion: 2016,
+        lugarNacimiento: `Romania`
+    },
+    colorPelo: `Castano`
+}
+
+let persona2 = {
+    nombre: `Martinez`,
+    apellido: `Ana`,
+    anyoNacimiento: 2003,
+    aficiones: [`leer`, `ver peliculas`, `pasear`, `dormir`],
+    dni: {
+        anyoExpedicion: 2020,
+        lugarNacimiento: `Madrid`
+    },
+    colorPelo: `Rojo`
+}
